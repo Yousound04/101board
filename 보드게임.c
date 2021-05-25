f704dca77c7c5291ed031a50c3134718a0355e8f
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-05-25 09:52:43 +0900

    금액기능 추가

--- 보드게임.c
+++ 보드게임.c
@@ -22,10 +22,12 @@ void main(void)
 	int position = 0;
 	//주사위
 	int dice;
+	//돈은 2억부터 시작 (좀 더 많은 숫자를 담기 위하여 long long으로 처리)
+	long long money = 200000000;
 
 	srand(time(0));
 
-	printf("현재위치 : %d\n\n\n", position);
+	printf("현재위치 : %d, 현재금액 : %lld \n\n\n", position, money);
 
 	//주사위는 1 ~ 6까지 나오게 설정
 	dice = rand() % 6 + 1;
@@ -33,7 +35,7 @@ void main(void)
 
 	//던진 주사위 값을 현재 위치에 적용
 	position += dice;
-	printf("현재위치 : %d\n\n", position);
+	printf("현재위치 : %d, 현재금액 : %lld \n\n", position);
 
 	//무한반복
 	while (1)
