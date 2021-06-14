8f0995c89ae1a30269301e201307d5d2c6cf11aa
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-06-14 14:55:46 +0900

    [문법연습] 구구단 2단 간단하게 2

---
+++ 구구단.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void){
+	printf("2 x 1 = %d\n", 2*1);
+	printf("2 x 2 = %d\n", 2*2);
+	printf("2 x 3 = %d\n", 2 * 3);
+	printf("2 x 4 = %d\n", 2 * 4);
+	printf("2 x 5 = %d\n", 2 * 5);
+	printf("2 x 6 = %d\n", 2 * 6);
+	printf("2 x 7 = %d\n", 2 * 7);
+	printf("2 x 8 = %d\n", 2 * 8);
+	printf("2 x 9 = %d\n", 2 * 9);
+}
