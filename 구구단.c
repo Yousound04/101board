caba38c356df0db38c5a3dc19468cbf4ae6705c4
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-06-15 09:10:13 +0900

    [문법연습] 구구단 2,3단 (이중 for문 사용)

--- 구구단.c
+++ 구구단.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-void main(void){
-
-	int dan = 2;
-	for (int i = 1; i<=9; i++)
+void main(void)
+{
+	
+	for (int dan = 2; dan <= 3; dan++)
 	{
-		printf("%d x %d = %d \n", dan, i, dan*i);
+		for (int i = 1; i <= 9; i++)
+		{
+			printf("%d x %d = %d \n", dan, i, dan * i);
+		}
 	}
-
 }
