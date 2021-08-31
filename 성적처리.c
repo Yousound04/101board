967aa14534b22144542ace7af9d7c9f0ffb6906d
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-08-31 10:12:34 +0900

    자료형태를 배열로 변환(refactoring)

--- 성적처리.c
+++ 성적처리.c
@@ -1,5 +1,30 @@
 ﻿#include <stdio.h>
 
+void cal_grade(int score);
+float average(int a, int b, int c);
+
+void main(void)
+{
+	float avg;
+	int subject[3];
+	printf("씨언어 점수를 입력하시오 : ");
+	scanf("%d", &subject);
+	cal_grade(C_score);
+
+	int java_score;
+	printf("자바 점수를 입력하시오 : ");
+	scanf("%d", &java_score);
+	cal_grade(java_score);
+
+	int CA_score;
+	printf("컴퓨터구조 점수를 입력하시오 : ");
+	scanf("%d", &CA_score);
+	cal_grade(CA_score);
+
+	avg = average(C_score, java_score, CA_score);
+	printf("세 과목의 평균은 %f입니다\n", avg);
+}
+
 // 입력받은 점수(score)에 대한 등급을 출력
 void cal_grade(int score)
 {
@@ -17,25 +42,8 @@ void cal_grade(int score)
 	printf("\n");
 }
 
-void main(void)
+// 세 점수의 평균을 반환
+float average(int a, int b, int c)
 {
-	int C_score;
-	printf("씨언어 점수를 입력하시오 : ");
-	scanf("%d", &C_score);
-
-	cal_grade(C_score);
-
-
-	int java_score;
-	printf("자바 점수를 입력하시오 : ");
-	scanf("%d", &java_score);
-
-	cal_grade(java_score);
-
-
-	int CA_score;
-	printf("컴퓨터구조 점수를 입력하시오 : ");
-	scanf("%d", &CA_score);
-
-	cal_grade(CA_score);
+	return (a + b + c) / 3.f;
 }
