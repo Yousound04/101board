9d79251c9a13945432175ebf47e07dcaf5461e05
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-09-27 13:59:03 +0900

    포인터 간접참조로 값을 변환

---
+++ 포인터 예제.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+void main(void)
+{
+	int num1 = 3;
+	int num2 = 4;
+	printf("%d\n", num1);
+	printf("%d\n\n", num2);
+
+	num1 = 30;
+	num2 = 40;
+	printf("%d\n", num1);
+	printf("%d\n\n", num2);
+
+	int num3 = 15;
+	int* ptr = &num3;
+	printf("%d\n", num3);
+	printf("%d\n", ptr);
+	printf("%d\n", *ptr);
+}
