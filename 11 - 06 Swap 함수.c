7c9c8ac27bbd86685fb07e778d65b4757ab76917
Yousound04 <83991018+Yousound04@users.noreply.github.com>
2021-10-26 10:02:03 +0900

    11- 06 Swap 함수

---
+++ 11 - 06 Swap 함수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void) {
+	int a = 10, b = 20;
+	printf("바꾸기 전 a:%d b%d \n", a, b);
+	Swap(a, b);
+	printf("바꾸기 후 a:d b:%d \n", a, b);
+}
+
+void Swao(int* num1, int* num2) {
+	int temp;
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
